ca39ccd17433cb7bca42903d04d27c9935d55657
tiger <44923556+eeeeaa@users.noreply.github.com>
2023-09-11 20:18:29 +0700

    Initialize base classes
    initialize base html,css,and js class

---
+++ js/etchASketch.js
@@ -0,0 +1,23 @@
+/***
+ * //GUI: 
+ * div containers and input dialog box
+ * //Input: 
+ * user mouse hover, user input grid size
+ * //Output: 
+ * fixed size square grid made of divs
+ * user can hover over grid and it will change color
+ * color should be randomize
+ * when user hover pass one grid multiple times, it gets 10% darker until it become black
+ * 
+ */
+
+
+const GRID_SIZE = 16;
+
+function generateGrid(){
+    for(let i = 0; i < GRID_SIZE;i++){
+        for(let j=0;j < GRID_SIZE;j ++){
+
+        }
+    }
+}
